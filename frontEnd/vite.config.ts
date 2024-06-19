1a615b4adafa132afe10f8f6f88cf7a8b6a71a2f
Adel <adelshaik@gmail.com>
2024-06-19 14:14:07 +0200

    workflow

--- frontEnd/vite.config.ts
+++ frontEnd/vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000  // Development server port
-  },
-  preview: {
-    port: 3000  // Preview server port
-  }
+ base: "/",
+ plugins: [react()],
+ preview: {
+  port: 3000,
+  strictPort: true,
+ },
+ server: {
+  port: 3000,
+  strictPort: true,
+  host: true,
+  origin: "http://0.0.0.0:3000",
+ },
 });
